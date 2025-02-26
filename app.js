916f37f87558a77babc747f85b663ae0aee57ab0
Juan Felipe Rojas <jrojash10040@universidadean.edu.co>
2025-02-25 20:06:52 -0500

    Creacion de proyecto y archivo app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json()); 
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando correctamente 🚀');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
